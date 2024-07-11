dc68bec79f8afb45e78bc7a211a7a5a842d38f12
moyw4ywa <167975867+moyw4ywa@users.noreply.github.com>
2024-07-11 13:03:31 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve the sign-up page
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// Endpoint for user signups
+app.post('/signup', (req, res) => {
+    const { email } = req.body;
+
+    // URL provided by Zapier for webhooks
+    const zapierWebhookUrl = 'https://hooks.zapier.com/hooks/catch/19332064/23p5ifo/'; // Replace with your Zapier webhook URL
+
+    axios.post(zapierWebhookUrl, { email })
+        .then(response => {
+            console.log('Data sent to Zapier:', response.data);
+            res.status(200).send('User signup data sent to Zapier');
+        })
+        .catch(error => {
+            console.error('Error sending data to Zapier:', error);
+            res.status(500).send('Error sending data to Zapier');
+        });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`App running on http://localhost:${port}`);
+});
