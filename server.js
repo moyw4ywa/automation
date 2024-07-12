5d04513534435021c299ace53d105af60c5a7a9b
Tonnel <154825284+T0nnel@users.noreply.github.com>
2024-07-12 23:38:35 +0300

    fixed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const path = require('path');
 
 const app = express();
 const port = 3000;
@@ -9,9 +10,12 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Serve the sign-up page
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Endpoint for user signups
@@ -19,7 +23,7 @@ app.post('/signup', (req, res) => {
     const { email } = req.body;
 
     // URL provided by Zapier for webhooks
-    const zapierWebhookUrl = 'https://hooks.zapier.com/hooks/catch/19332064/23penfq/'; 
+    const zapierWebhookUrl = "https://hooks.zapier.com/hooks/catch/19332064/23pw97n/"; // Replace with your Zapier webhook URL
 
     axios.post(zapierWebhookUrl, { email })
         .then(response => {
